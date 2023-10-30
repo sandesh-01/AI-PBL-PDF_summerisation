16b79e994ccb82952b5cb8d0f755588f0d51146f
SANDESH BUCHKUL <101316712+sandesh-01@users.noreply.github.com>
2023-10-30 16:25:21 +0530

    renamed

--- jupyter nb/htmlTemplates.py
+++ jupyter nb/htmlTemplates.py
@@ -28,7 +28,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="pipe.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="bot.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="bird.png">
+        <img src="user.png">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
